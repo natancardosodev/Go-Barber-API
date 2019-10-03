185a25b6942bfecbd11e84f5b03f627f8668eb6a
Natan Cardoso <natan@voxtecnologia.com.br>
2019-10-03 17:57:24 -0300

    ADD: Autenticação com JWT

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+
+import SessionControler from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
+routes.get('/session', SessionControler.store);
 routes.post('/users', UserController.store);
 
 export default routes;
