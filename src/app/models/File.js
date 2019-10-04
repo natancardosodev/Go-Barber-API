0285061f1b448339f5527699c083d8c09f0cadd2
Natan Cardoso <natan@voxtecnologia.com.br>
2019-10-04 14:32:55 -0300

    ADD: Upload de arquivos com Multer

---
+++ src/app/models/File.js
@@ -0,0 +1,17 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Path extends Model {
+    static init(sequelize) {
+        super.init({
+            name: Sequelize.STRING,
+            path: Sequelize.STRING,
+        },
+        {
+            sequelize,
+        });
+
+        return this;
+    }
+}
+
+export default Path;
---
+++ src/config/multer.js
@@ -0,0 +1,28 @@
+import multer from 'multer';
+import crypto from 'crypto';
+import { extname, resolve } from 'path';
+
+export default {
+    storage: multer.diskStorage({
+        destination: resolve(__dirname, '..', '..', 'temp', 'uploads'),
+        filename: (req, file, callback) => {
+            crypto.randomBytes(16, (err, raw) => {
+                if (err) return callback(err);
+
+                return callback(null, raw.toString('hex') + extname(file.originalname));
+            });
+        },
+    }),
+    limits: {
+        fileSize: 2 * 1024 * 1024,
+    },
+    fileFilter: (req, file, callback) => {
+        const allowedMimes = ['image/jpeg', 'image/jpg', 'image/png'];
+
+        if (allowedMimes.includes(file.mimetype)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Invalid file type'));
+        }
+    },
+};
