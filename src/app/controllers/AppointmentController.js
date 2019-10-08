20bd8ac7a853ee385139ac6c1b8b54c639998a47
Natan Cardoso <natan@voxtecnologia.com.br>
2019-10-08 17:52:42 -0300

    Cancelamento de agendamento e variaveis de ambiente

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,13 +1,13 @@
 import * as Yup from 'yup';
 import {
-    startOfHour, parseISO, isBefore, format,
+    startOfHour, parseISO, isBefore, format, subHours,
 } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
-import { subHours } from 'date-fns/esm';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
+import Mail from '../../lib/Mail';
 
 class ProviderController {
     async index(req, res) {
@@ -100,7 +100,15 @@ class ProviderController {
     }
 
     async delete(req, res) {
-        const appointment = await Appointment.findByPk(req.params.id);
+        const appointment = await Appointment.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    as: 'provider',
+                    attributes: ['name', 'email'],
+                },
+            ],
+        });
 
         if (appointment.user_id !== req.userId) {
             return res.status(401).json({
@@ -119,6 +127,12 @@ class ProviderController {
         appointment.canceled_at = new Date();
 
         await appointment.save();
+
+        await Mail.sendMail({
+            to: `${appointment.provider.name} <${appointment.provider.email}>`,
+            subject: 'Agendamento cancelado',
+            text: 'Você tem um novo cancelamento',
+        });
 
         return res.json(appointment);
     }
