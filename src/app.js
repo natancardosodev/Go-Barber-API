6e420ee11be05d50da17fc69c35fbc9b4a023ea0
Natan Cardoso <natan@voxtecnologia.com.br>
2019-10-09 17:36:23 -0300

    Sentry para logar erros e ExceptionHandler

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import 'dotenv/config';
-
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
 import express from 'express';
+import 'express-async-errors';
 import path from 'path';
 import routes from './routes';
+import sentryConfig from './config/sentry';
 
 import './database';
 
@@ -10,11 +13,15 @@ class App {
     constructor() {
         this.server = express();
 
+        Sentry.init(sentryConfig);
+
         this.middlewares();
         this.routes();
+        this.exceptionHandler();
     }
 
     middlewares() {
+        this.server.use(Sentry.Handlers.requestHandler());
         this.server.use(express.json());
         this.server.use(express.urlencoded({ extended: false }));
         this.server.use(
@@ -25,6 +32,19 @@ class App {
 
     routes() {
         this.server.use(routes);
+        this.server.use(Sentry.Handlers.requestHandler());
+    }
+
+    exceptionHandler() {
+        this.server.use(async (err, req, res, next) => {
+            if (process.env.NODE_ENV === 'development') {
+                const errors = await new Youch(err, req).toJSON();
+
+                return res.status(500).json(errors);
+            }
+
+            return res.status(500).json({ error: 'Internal server error' });
+        });
     }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+    dsn: 'https://43aeb3341b044b9b906fbf90b5a88e01@sentry.io/1774935',
+};
